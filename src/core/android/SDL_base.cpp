3931a918321ec4bf17a5c8ca7324404c28f4de51
LCID Fire <lcid-fire@gmx.net>
2012-01-24 03:06:46 +0100

    Add Base class which contains Log functions and Context

--- src/core/android/SDL_base.cpp
+++ src/core/android/SDL_base.cpp
@@ -1,1 +1,73 @@
+
+#include "include.h"
 #include "SDL_base.h"
+#include <android/log.h>
+
+Base::Base(Context& context):
+	mContext(context)
+{
+}
+
+void Base::LOGV(const char* message)
+{
+	Log(ANDROID_LOG_VERBOSE, message);
+}
+
+void Base::LOGE(const char* message)
+{
+	Log(ANDROID_LOG_ERROR, message);
+}
+
+void Base::LOGW(const char* message)
+{
+	Log(ANDROID_LOG_WARN, message);
+}
+
+void Base::LOGV(const std::string& message)
+{
+	LOGV(message.c_str());
+}
+
+void Base::LOGE(const std::string& message)
+{
+	LOGE(message.c_str());
+}
+
+void Base::LOGW(const std::string& message)
+{
+	LOGW(message.c_str());
+}
+
+void Base::LOGVF(const char *fmt, ...)
+{
+	va_list vl;
+	va_start(vl, fmt);
+	Log(ANDROID_LOG_VERBOSE, fmt, vl);
+	va_end(vl);
+}
+
+void Base::LOGEF(const char *fmt, ...)
+{
+	va_list vl;
+	va_start(vl, fmt);
+	Log(ANDROID_LOG_ERROR, fmt, vl);
+	va_end(vl);
+}
+
+void Base::LOGWF(const char *fmt, ...)
+{
+	va_list vl;
+	va_start(vl, fmt);
+	Log(ANDROID_LOG_WARN, fmt, vl);
+	va_end(vl);
+}
+
+void Base::Log(int prio, const char* message)
+{
+	__android_log_print(prio, "SDL", message);
+}
+
+void Base::Log(int prio, const char* fmt, va_list ap)
+{
+	__android_log_vprint(prio, "SDL", fmt, ap);
+}
--- src/core/android/SDL_base.h
+++ src/core/android/SDL_base.h
@@ -2,4 +2,31 @@
 #ifndef SDL_ANDROID_BASE_H_
 #define SDL_ANDROID_BASE_H_
 
+namespace SDL{ namespace Android{
+class Base
+{
+protected:
+	Base(Context& context);
+
+	void LOGV(const char* message);
+	void LOGE(const char* message);
+	void LOGW(const char* message);
+
+	void LOGV(const std::string& message);
+	void LOGE(const std::string& message);
+	void LOGW(const std::string& message);
+
+	void LOGVF(const char *fmt, ...);
+	void LOGEF(const char *fmt, ...);
+	void LOGWF(const char *fmt, ...);
+
+	Context& mContext;
+
+private:
+	void Log(int prio, const char* message);
+	void Log(int prio, const char* fmt, va_list ap);
+};
+}}
+
+
 #endif // SDL_ANDROID_BASE_H_
